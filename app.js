002e788177a4dbf5d3c03110526c353a0aee0db4
Anastasia <www.nastya97@yandex.ru>
2023-04-10 18:20:30 +0300

    feat: add models

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
 // подключили экспресс
 const express = require('express');
 
+const mongoose = require('mongoose');
+
 const { PORT = 3000 } = process.env;
 const app = express(); // создали приложение методом экспресс
 
-// передадим обработчик
+// подключаем mongoose
+mongoose
+  .connect('mongodb://localhost:27017/mestodb')
+  .then(() => console.log('Connected to mongoose!'));
+
+// подключили приложение к порту 3000
 app.listen(PORT, () => {
   // листен - метод чтобы понять с какого сервера надо принимать сообщения
   // Если всё работает, консоль покажет, какой порт приложение слушает
-  console.log(`App listening on port ${PORT}`);
+  console.log(`Connected to the port: ${PORT}`);
 });
