54438bc9aa33caf92728d71b8cb315dc875931d9
Anastasia <www.nastya97@yandex.ru>
2023-04-28 21:35:26 +0300

    feat: test5

--- app.js
+++ app.js
@@ -35,7 +35,7 @@ app.post(
       password: Joi.string().required().min(8),
       name: Joi.string().min(2).max(30),
       about: Joi.string().min(2).max(30),
-      avatar: Joi.string().pattern(regExp),
+      avatar: Joi.string().regex(regExp),
     }),
   }),
   createUser
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line operator-linebreak
 const regExp =
-  /http(s)?:\/\/(www.)?[a-zA-Z0-9\-._~:/?#[]@!$&'()\*+,;=]*\.[a-zA-Z0-9\-._~:/?#[]@!$&'()\*+,;=]*/;
+  /http(s)?:\/\/(www\.)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]+\.[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]+/;
 
 module.exports = {
   regExp,
