60ca28e707b999815f3cb60814058d1e0a0abe33
Anastasia <www.nastya97@yandex.ru>
2023-05-03 14:04:49 +0300

    hotfix: final changes

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,7 +5,7 @@ const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    throw new UnauthorizedError('Необходима авторизация.');
+    return next(new UnauthorizedError('Необходима авторизация.'));
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -16,7 +16,7 @@ const auth = (req, res, next) => {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
     // отправим ошибку, если не получилось
-    next(new UnauthorizedError('Необходима авторизация.'));
+    return next(new UnauthorizedError('Необходима авторизация.'));
   }
 
   req.user = payload; // записываем пейлоуд в объект запроса
